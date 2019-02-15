4cdfa0f36b68baaf9ae88d1494b291d3434c0e33
Fillan Lee <fillanlee@aol.com>
2019-02-15 03:24:02 +0000

    Automatically backed up by Learn

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -4,7 +4,7 @@ def my_select(collection)
  new_array = []
  
  while counter <  collection.length 
- new_array << yield(collection[counter].even?)
+ new_array << yield(collection[counter])
 
  counter +=1 
  end
