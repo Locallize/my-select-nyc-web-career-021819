b391ded2a1f0a3391feee28ffbbdb8b3c9aa0eb8
Fillan Lee <fillanlee@aol.com>
2019-02-15 03:19:16 +0000

    Automatically backed up by Learn

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -15,12 +15,12 @@ end
 
 
 
-def my_collect (array)
-counter = 0  
-new_array = []
-# upcase_array = []
+# def my_collect (array)
+# counter = 0  
+# new_array = []
+# # upcase_array = []
 
-while counter < array.length
-new_array << yield(array[counter].split(" ").first)
-counter += 1
-end
+# while counter < array.length
+# new_array << yield(array[counter].split(" ").first)
+# counter += 1
+# end
