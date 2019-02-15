fbb9d5c7c29326d7f4b24c5c92d1fe6e71823bdc
Fillan Lee <fillanlee@aol.com>
2019-02-15 03:08:16 +0000

    Automatically backed up by Learn

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,3 +1,26 @@
 def my_select(collection)
  # your code here!
+ counter = 0
+ new_array = []
+ 
+ while counter <  collection.length 
+ item = collection[counter]
+  yield new_array << item.even?
+ 
+ counter +=1 
+ end
+ 
+ new_array
+end
+
+
+
+def my_collect (array)
+counter = 0  
+new_array = []
+# upcase_array = []
+
+while counter < array.length
+new_array << yield(array[counter].split(" ").first)
+counter += 1
 end
